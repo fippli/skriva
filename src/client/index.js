ebea83a4c18bbb3753857463dffbb0ce0b3d06e3
Filip Johansson <filip.johansson@protonmail.com>
2020-05-31 23:09:05 +0200

    updates

--- src/client/index.js
+++ src/client/index.js
@@ -2,20 +2,17 @@ const { ipcRenderer } = require("electron");
 
 // !
 const setTextarea = (text) => {
-  console.log("textarea update");
   document.getElementById("textarea").value = text;
 };
 
 // !
 const setFilePath = (filePath) => {
-  console.log("file path update");
   document.getElementById("file-path").textContent = filePath;
 };
 
 // !
 const stateChange = (_, state) => {
   const { fileContent, filePath } = state;
-  console.log("State change on client", fileContent, filePath);
   setTextarea(fileContent);
   setFilePath(filePath);
 };
--- src/utils/sendToClient.js
+++ src/utils/sendToClient.js
@@ -4,9 +4,7 @@ const last = require("@codewell/last");
 // Send data to the client on a
 // message channel
 const sendToClient = (data) => {
-  console.log("Sending new state!!!");
   const window = last(BrowserWindow.getAllWindows());
-  console.log(window);
   window.webContents.send("STATE_CHANGE", data);
 };
 
