33f5e2a7df6c428ad9402b279c7cc94b11585756
Filip Johansson <filip.johansson@protonmail.com>
2020-09-13 13:41:31 +0200

    Fix bug that prevented file saves

--- src/file/saveAs.js
+++ src/file/saveAs.js
@@ -2,21 +2,23 @@ const { dialog } = require("electron");
 const isDefined = require("@codewell/is-defined");
 const { readState, dispatch } = require("../state");
 const writeFile = require("../utils/writeFile");
+const sendToClient = require("../utils/sendToClient");
 
 // Save filecontent and select a
 // file path
 const saveAs = () => {
-  const dialogData = dialog.showSaveDialogSync({
+  const filePath = dialog.showSaveDialogSync({
     properties: ["createDirectory"],
   });
 
-  if (!isDefined(dialogData)) {
+  if (!isDefined(filePath)) {
     dispatch({ filePath: null });
   } else {
-    const [filePath] = dialogData;
     dispatch({ filePath });
-    const { fileContent } = readState();
+    const state = readState();
+    const { fileContent } = state;
     writeFile(filePath)(fileContent);
+    sendToClient(state);
   }
 };
 
